644af0899225be6de627be4cd05e0af459839bc7
mikexiao007 <mikexiao007@163.com>
2017-07-21 09:42:22 +0800

    2017-07-20 version 1.0

---
+++ camera_show/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: xiaoyh
+ *
+ * Created on July 20, 2017, 3:48 PM
+ */
+
+#include <cstdlib>
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <string.h>  
+
+using namespace std;
+
+/*
+ * 
+ */
+/*
+int main(int argc, char** argv) {
+
+    printf("hello world\n");
+    return 0;
+}*/
+
